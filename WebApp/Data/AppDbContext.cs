0ceaa4880adc5f08c05bbb4351477a33f8b706b1
fahmidanial <fahmidanial9091@gmail.com>
2024-06-20 11:26:21 +0800

    migration berjaya

---
+++ WebApp/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+		public DbSet<Customer> Customers { get; set; } = default!;
+	}
+}
